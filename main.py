a30845210b64cdba8cd89b074b596f5528b3990f
iBarakaa <isaacbaraka1@gmail.com>
2023-02-06 11:40:44 +0300

    Equity Statistics clickable is functional

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
 # the selenium version used is v. 4.0.0.b4
 
 # path for chromedriver
 # conversion of backslash to forward slashes
+
 s = Service("C:/SeleniumDrivers/chromedriver.exe")
 
 # we give our driver a path
@@ -13,6 +15,5 @@ driver = webdriver.Chrome(service= s)
 # opening website
 driver.get("https://www.nse.co.ke/dataservices/market-statistics/")
 
-# # use of the Xpaths
-# # you can procure the Xpath throuh
-# driver.find_element_by_xpath()
+equity_statistics = driver.find_element(by="xpath", value="/html/body/div[1]/div/div[3]/div[2]/div/div/div[1]/div[2]/div/div/div/div/div/ul/li[2]/a/span")
+equity_statistics.click()
