7bc2116dd668bc21d3f87fb588f78af3b3708d96
iBarakaa <isaacbaraka1@gmail.com>
2023-02-06 16:27:29 +0300

    Attempts to collect data from dropdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
 import time
 
 # the selenium version used is v. 4.0.0.b4
@@ -14,6 +15,29 @@ driver = webdriver.Chrome(service= s)
 
 # opening website
 driver.get("https://www.nse.co.ke/dataservices/market-statistics/")
+time.sleep(10)
 
+# navigation to the equity statistics resources
 equity_statistics = driver.find_element(by="xpath", value="/html/body/div[1]/div/div[3]/div[2]/div/div/div[1]/div[2]/div/div/div/div/div/ul/li[2]/a/span")
 equity_statistics.click()
+time.sleep(5)
+
+es_dropdown = Select(driver.find_element(by="xpath", value="/html/body/div[1]/div/div[3]/div[2]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[1]/div[1]/form/select"))
+
+# entries within the selected dropdown
+options = [option.text for option in es_dropdown.options]
+
+for option in options:
+    if option != "Select Sector":
+        try:
+            es_dropdown.select_by_visible_text(option)
+            print(option)
+            time.sleep(5)
+        except:
+            continue
+
+
+# shares = driver.find_elements(by="xpath", value="/html/body/div[1]/div/div[3]/div[2]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div/table/tbody/tr")
+
+# for share in shares:
+#     print(shares.text)
